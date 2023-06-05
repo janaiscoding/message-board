19f2b62a31cb043ff684e2460b6b81fb47efd84b
janaiscoding <jana.istrate@gmail.com>
2023-06-05 16:16:34 +0300

    Create logic for POST Method

---
+++ README.md
@@ -0,0 +1,1 @@
+# MESSAGE BOARD APP - Express / Mongoose
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* POST user message*/
+router.post("/new", (req, res, next) => {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
 });
 
 module.exports = router;
